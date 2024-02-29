730463156b42580ad017cee5df4519e7724144a6
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-01 04:10:37 +0800

    chore:build

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,27 @@
 import { defineConfig } from 'vite'
-import path from 'node:path'
 import { build } from "vite";
 import cp from 'vite-plugin-cp';
-// make library list, the name for folder
+/*
+inlineDynamicImports: false,
+experimentalMinChunkSize: 999999999,
+manualChunks: (id) => {
+  return library;
+}*/
+const externalAll = ["fs", "path", "electron", "express", /node:/];
 export function BuildPlugin() {
-  const librarys = ["main", "preload", "renderer"];
-
+  const librarys = ["main", "preload"];
 
   librarys.forEach(async (library) => {
     await build({
       configFile: false,
       build: {
         lib: {
-          formats: ["cjs", "es"],
+          formats: ["cjs"],
           entry: { [library]: `src/${library}.ts` }
         },
-        assetsDir: "",
         emptyOutDir: false,
         rollupOptions: {
-          external: ['electron', 'express', "node:v8", "node:vm", "node:fs", "node:path", "fs", "path"],
-          output: {
-            /*inlineDynamicImports: false,
-            experimentalMinChunkSize: 999999999,
-            manualChunks: (id) => {
-              return library;
-            }*/
-          }
+          external: externalAll,
         }
       }
     });
@@ -38,11 +34,11 @@ export default defineConfig({
   build: {
     emptyOutDir: false,
     lib: {
-      formats: ['cjs', 'es'],
-      entry: path.join(__dirname, '/script/build.ts'),
+      formats: ['es'],
+      entry: { ["renderer"]: `src/renderer.ts` },
     },
     rollupOptions: {
-      external: ["node:v8", "node:vm", "node:fs", "node:path", "fs", "path"]
+      external: externalAll
     }
   },
   plugins: [cp({ targets: [{ src: './package.json', dest: 'dist' }, { src: './manifest.json', dest: 'dist' }] })]
