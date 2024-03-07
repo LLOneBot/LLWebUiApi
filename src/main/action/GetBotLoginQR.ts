aa9060ea1c18d4292555f0a94debf057fa0b1257
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-07 22:00:26 +0800

    fix:handle

--- src/main/action/GetBotLoginQR.ts
+++ src/main/action/GetBotLoginQR.ts
@@ -1,9 +1,13 @@
 import { BaseAction } from "./BaseAction";
 import { ActionName } from "./types";
 
-export class GetBotLoginQR extends BaseAction<null, any> {
+export interface WebStateResponse {
+    name: string;
+}
+
+export class GetBotLoginQR extends BaseAction<void, WebStateResponse> {
     public actionName: string = ActionName.GetBotLoginQR;
-    public async _handle(_payload: any): Promise<any>  {
-        return "GetBotLoginQR";
+    public async _handle(_payload: void): Promise<WebStateResponse> {
+        return { name: "GetBotLoginQR" };
     }
 }
