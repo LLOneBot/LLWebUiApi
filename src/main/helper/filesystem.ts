df7f148bbb5b97a6d6b7cfe696d79b6d3bbeeb24
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-09 16:35:14 +0800

    feat:filesystem

---
+++ src/main/helper/filesystem.ts
@@ -0,0 +1,15 @@
+class filesystem {
+    static basePath: string;
+    static init(base: string) {
+        filesystem.basePath = base;
+    }
+    static listFile(_path: string) {
+
+    }
+    static deletePath(_path: string) {
+
+    }
+    static searchFile() {
+
+    }
+}
