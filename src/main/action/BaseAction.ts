e2f4f0efa94f8193a8eb5ea4d36dddab11e1e26b
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-09 18:01:10 +0800

    refactor:ActionCheck

--- src/main/action/BaseAction.ts
+++ src/main/action/BaseAction.ts
@@ -1,9 +1,22 @@
 import { ResponseReturn } from "./types";
 import { JsonResponse } from "../server/JsonResponse";
+import { DataClass } from "../helper/data";
+import { WebState, WebStateCode } from "../../common/types";
 export class BaseAction<PayloadType, ReturnDataType> {
     public actionName: string = "";
+    protected async check(_payload: PayloadType): Promise<boolean> {
+        // 大部分Api 登录后使用 不登陆 就没必要
+        if ((DataClass.getInstance().get("WebUiApiState") as WebState).WorkState == WebStateCode.WAIT_LOGIN) {
+            return false;
+        }
+        return true;
+    }
     public async handle(payload: PayloadType): Promise<ResponseReturn<ReturnDataType | null>> {
         let resData: ReturnDataType;
+        const result = await this.check(payload);
+        if (!result) {
+            return JsonResponse.error("This RequenstNot Pass Check");
+        }
         try {
             resData = await this._handle(payload);
         } catch (e: any) {
--- src/main/action/GetBotLoginQR.ts
+++ src/main/action/GetBotLoginQR.ts
@@ -9,9 +9,15 @@ export interface WebStateResponse {
 
 export class GetBotLoginQR extends BaseAction<void, WebStateResponse> {
     public actionName: string = ActionName.GetBotLoginQR;
+    protected async check(_payload: void): Promise<boolean> {
+        if ((DataClass.getInstance().get("WebUiApiState") as WebState).WorkState != WebStateCode.WAIT_LOGIN) {
+            return false;
+        }
+        return true;
+    }
     public async _handle(_payload: void): Promise<WebStateResponse> {
         const base64Image = DataClass.getInstance().get("QRCODE_BASE64");
-        if ((DataClass.getInstance().get("WebUiApiState") as WebState).WorkState != WebStateCode.WAIT_LOGIN || base64Image === "") {
+        if (base64Image === "") {
             throw new Error('Is Running Or Error !');
         }
         return { base64Image: base64Image };
