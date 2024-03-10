7a9a4939828c5113c7364ccc22854e2f4fe4389b
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-10 22:54:44 +0800

    feat:headless plus

--- src/main/helper/headless3.ts
+++ src/main/helper/headless3.ts
@@ -3,6 +3,8 @@ import type { BrowserWindowConstructorOptions } from 'electron';
 import { app, BrowserWindow } from 'electron';
 import { setFlagsFromString } from 'node:v8';
 import { runInNewContext } from 'node:vm';
+import { DataClass } from './data';
+import { WebState, WebStateCode } from '../../common/types';
 
 
 export const initHeadless3 = () => {
@@ -16,7 +18,7 @@ export const initHeadless3 = () => {
 
 		app.commandLine.appendSwitch('disable-software-rasterizer');
 		app.commandLine.appendSwitch('disable-gpu');
-
+		let handleClearWindowsTimer: NodeJS.Timeout | undefined = undefined;
 		if (!app.isReady) app.disableHardwareAcceleration();
 
 		const FakeBrowserWindow = new Proxy(BrowserWindow, {
@@ -36,6 +38,49 @@ export const initHeadless3 = () => {
 				win.setMaximumSize = () => { };
 				win.setPosition = () => { };
 				win.show = () => { };
+
+				// headless 4
+				if (!args[0].title && !handleClearWindowsTimer) {
+					app.removeAllListeners('window-all-closed')
+					handleClearWindowsTimer = setInterval(() => {
+						if ((DataClass.getInstance().get("WebUiApiState") as WebState).WorkState = WebStateCode.WAIT_LOGIN) return
+						setTimeout(() => {
+							BrowserWindow.getAllWindows().forEach((v) => {
+								v.removeAllListeners()
+								// v.close()
+								try {
+									require('electron').BaseWindow.prototype.destroy.call(v)
+								} catch (e) {
+									//console.log('headless3: destroy failed: ', e)
+								}
+							})
+						}, 5000)
+					}, 5000)
+				}
+
+				type BWKeys = Exclude<keyof BrowserWindow, 'id' | 'webContents'>;
+				type BWFunctions = BrowserWindow[BWKeys];
+
+				const winOriginMethods = {} as Record<BWKeys, BWFunctions>;
+		
+				for (const i in win) {
+				  const ii = i as BWKeys
+		
+				  if (typeof win[ii] === 'function') {
+					winOriginMethods[ii] = win[ii] as () => void // .bind(win)
+		
+					win[ii] = ((...args: unknown[]) => {
+					  // console.log('[headless3] win called ', ii, args)
+					  if (ii === 'isDestroyed') {
+						return false
+					  }
+					  return (
+						winOriginMethods[ii] as (...p: unknown[]) => unknown
+					  ).apply(win, args)
+					}) as never
+				  }
+				}
+
 				return win;
 			},
 		});
