c24ae6fed3e88b7c2f0ee31874ef006cd609fd78
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-10 15:17:14 +0800

    fix:path

--- src/main/helper/utils.ts
+++ src/main/helper/utils.ts
@@ -4,7 +4,7 @@ const DATA_DIR = global.LiteLoader.plugins['WebUiApi'].path.data;
 const PLUGIN_DIR = global.LiteLoader.plugins['WebUiApi'].path.plugin;
 const ALL_PLUGIN_DIR = path.resolve(PLUGIN_DIR, '..');
 const ALL_DATA_DIR = path.resolve(DATA_DIR, '..');
-const LITELOADER_DIR = path.resolve(PLUGIN_DIR, '..');
+const LITELOADER_DIR = path.resolve(ALL_PLUGIN_DIR, '..');
 export {
 	DATA_DIR,
 	PLUGIN_DIR,
