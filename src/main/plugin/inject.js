d77fba74e95f860993eb79554f2f7716231d4887
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-10 15:09:42 +0800

    feat:inject plugin

---
+++ src/main/plugin/inject.js
@@ -0,0 +1,35 @@
+// 注入插件
+import { onSettingWindowCreated } from './renderer.js';
+
+(async () => {
+  const settingView = document.querySelector('#app');
+  const FakeElectron = {
+    ipcRenderer: {
+      invoke: (channel, ...args) => new Promise((res, rej) => {
+        console.log('[Electron][IPC][Invoke]', channel, args);
+      }),
+      send: (channel, ...args) => new Promise((res, rej) => {
+        console.log('[Electron][IPC][Send]', channel, args);
+      }),
+    },
+    contextBridge: {
+      exposeInMainWorld: (slug, obj) => {
+        window[slug] = { ...obj };
+      }
+    }
+  };
+  const FakeRequire = (moduleName) => {
+    if (moduleName === 'electron') return FakeElectron;
+    else return {};
+  }
+  
+  window.__FAKE_REQUIRE__ = FakeRequire;
+  
+  const PreloadRes = await fetch('./preload.js');
+  const PreloadRaw = await PreloadRes.text();
+  const PreloadJS = `const require = window.__FAKE_REQUIRE__;${PreloadRaw}`;
+
+  eval(PreloadJS);
+
+  onSettingWindowCreated(settingView);
+})();
