ea16ec46f28beb674357b2ec0f83beb57ada5ce4
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-12 12:03:40 +0800

    feat:re headless

--- src/main/helper/headless3.ts
+++ src/main/helper/headless3.ts
@@ -18,7 +18,6 @@ export const initHeadless3 = () => {
 
 		app.commandLine.appendSwitch('disable-software-rasterizer');
 		app.commandLine.appendSwitch('disable-gpu');
-		let handleClearWindowsTimer: NodeJS.Timeout | undefined = undefined;
 		if (!app.isReady) app.disableHardwareAcceleration();
 
 		const FakeBrowserWindow = new Proxy(BrowserWindow, {
@@ -38,49 +37,6 @@ export const initHeadless3 = () => {
 				win.setMaximumSize = () => { };
 				win.setPosition = () => { };
 				win.show = () => { };
-
-				// headless 4
-				if (!args[0].title && !handleClearWindowsTimer) {
-					app.removeAllListeners('window-all-closed')
-					handleClearWindowsTimer = setInterval(() => {
-						if ((DataClass.getInstance().get("WebUiApiState") as WebState).WorkState = WebStateCode.WAIT_LOGIN) return
-						setTimeout(() => {
-							BrowserWindow.getAllWindows().forEach((v) => {
-								v.removeAllListeners()
-								// v.close()
-								try {
-									require('electron').BaseWindow.prototype.destroy.call(v)
-								} catch (e) {
-									//console.log('headless3: destroy failed: ', e)
-								}
-							})
-						}, 5000)
-					}, 5000)
-				}
-
-				type BWKeys = Exclude<keyof BrowserWindow, 'id' | 'webContents'>;
-				type BWFunctions = BrowserWindow[BWKeys];
-
-				const winOriginMethods = {} as Record<BWKeys, BWFunctions>;
-		
-				for (const i in win) {
-				  const ii = i as BWKeys
-		
-				  if (typeof win[ii] === 'function') {
-					winOriginMethods[ii] = win[ii] as () => void // .bind(win)
-		
-					win[ii] = ((...args: unknown[]) => {
-					  // console.log('[headless3] win called ', ii, args)
-					  if (ii === 'isDestroyed') {
-						return false
-					  }
-					  return (
-						winOriginMethods[ii] as (...p: unknown[]) => unknown
-					  ).apply(win, args)
-					}) as never
-				  }
-				}
-
 				return win;
 			},
 		});
