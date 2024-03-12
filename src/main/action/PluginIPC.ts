1ad2fb671b44728db5345af52da63795006e4e7a
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-12 11:21:29 +0800

    feat:PluginIPC

--- src/main/action/PluginIPC.ts
+++ src/main/action/PluginIPC.ts
@@ -11,8 +11,17 @@ export interface PluginIPCPayload {
 
 export class PluginIPC extends BaseAction<PluginIPCPayload, PluginIPCResponse> {
     public actionName: string = ActionName.PluginIPC;
-    public async _handle(_payload: PluginIPCPayload): Promise<PluginIPCResponse> {
-
+    public async _handle(payload: PluginIPCPayload): Promise<PluginIPCResponse> {
+        if (payload.type == PluginIpcType.Listen) {
+            // 注册监听
+        }
+        if (payload.type == PluginIpcType.Send) {
+            // 单向IPC
+        }
+        if (payload.type == PluginIpcType.Invoke) {
+            // 双向IPC
+        }
         return { message: "PluginIPC Ok!" };
+
     }
 }
