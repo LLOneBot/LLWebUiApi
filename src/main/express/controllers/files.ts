dcc0b6ea3406ad2155ff16991b43d590ae49d981
Misa Liu <misaliu@misaliu.top>
2024-03-13 23:25:06 +0800

    feat: Add path verify

--- src/main/express/controllers/files.ts
+++ src/main/express/controllers/files.ts
@@ -1,9 +1,25 @@
 import fs from 'fs';
+import { resolve } from 'path';
 import { FileType } from '@/main/action/types';
 import { Request, Response } from 'express';
 
+const BASE_DIR = LiteLoader.path.root;
+const verifyPath = (path: string) => {
+  const newPath = resolve(BASE_DIR, path);
+  if (newPath.indexOf(BASE_DIR) !== 0) return false;
+  else return true;
+}
+
 export const Delete = (req: Request, res: Response) => {
-  const { type, path } = req.body;
+  const { type } = req.body;
+  const path = resolve(BASE_DIR, req.body.path);
+
+  if (!verifyPath(path)) {
+    return res.status(400)
+      .json({
+        msg: 'Invaild path'
+      });
+  }
 
   try {
     if (!fs.existsSync(path)) {
@@ -28,7 +44,14 @@ export const Delete = (req: Request, res: Response) => {
 }
 
 export const Get = (req: Request, res: Response) => {
-  const { path } = req.body;
+  const path = resolve(BASE_DIR, req.body.path);
+
+  if (!verifyPath(path)) {
+    return res.status(400)
+      .json({
+        msg: 'Invaild path'
+      });
+  }
 
   if (!fs.existsSync(path)) {
     return res.status(400)
