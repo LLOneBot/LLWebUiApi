c333093eea2184d322f3a976029a90d0ec8db9ca
Misa Liu <misaliu@misaliu.top>
2024-03-14 22:58:52 +0800

    feat: Finishing `LiteLoader` API in `/plugin/iframe`

--- src/main/express/routes/plugin-inject.ts
+++ src/main/express/routes/plugin-inject.ts
@@ -4,5 +4,7 @@ import * as PluginInject from '@/main/express/controllers/plugin-inject';
 const router = Router();
 
 router.get('/js', PluginInject.GetJs);
+router.get('/config/:pluginSlug', PluginInject.GetConfig);
+router.post('/config/:pluginSlug', PluginInject.SetConfig);
 
 export { router as PluginInjectRouter };
--- static/plugin/llapi.js
+++ static/plugin/llapi.js
@@ -1,1 +1,50 @@
 ;window.LiteLoader={ LLDATA };
+window.LiteLoader.api = {
+  openPath(path) {
+    console.log('[LiteLoader][OpenPath]', path);
+  },
+  openExternal(uri) {
+    console.log('[LiteLoader][OpenExternal]', uri);
+    window.open(uri, '_blank');
+  },
+  disablePlugin(slug) {
+    console.log('[LiteLoader][DisablePlugin]', uri);
+  },
+
+  config: {
+    get(slug, defaultConfig = {}) {
+      console.log('[LiteLoader][Config][Get]', slug, defaultConfig);
+      return new Promise(async (_res, _rej) => {
+        const res = await axios({
+          method: 'GET',
+          url: `/plugin/inject/config/${slug}`,
+        });
+        const resBody = res.data;
+
+        if (res.status !== 200 || resBody.msg !== 'ok') {
+          return _rej(resBody);
+        }
+
+        const result = { ...defaultConfig, ...resBody.data };
+        _res(result);
+      });
+    },
+    set(slug, newConfig) {
+      console.log('[LiteLoader][Config][Set]', slug, newConfig);
+      return new Promise(async (_res, _rej) => {
+        const res = await axios({
+          method: 'POST',
+          url: `/plugin/inject/config/${slug}`,
+          data: newConfig,
+        });
+        const resBody = res.data;
+
+        if (res.status !== 200 || resBody.msg !== 'ok') {
+          return _rej(resBody);
+        }
+
+        _res();
+      });
+    },
+  }
+};
