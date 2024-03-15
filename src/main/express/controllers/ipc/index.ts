970c4e91ef08bca1dc522749ccc48c8f2d924c84
Misa Liu <misaliu@misaliu.top>
2024-03-16 02:05:10 +0800

    refactor: WebSocket structure

--- src/main/express/controllers/ipc/index.ts
+++ src/main/express/controllers/ipc/index.ts
@@ -1,31 +1,6 @@
 import { WebsocketRequestHandler } from 'express-ws';
 
-export const IPCHandler: WebsocketRequestHandler = (ws, req) => {
-  const sendMsg = (msg: any) => ws.send(JSON.stringify(msg));
-  const { slug: pluginSlug } = req.query;
-
-  if (!pluginSlug || !global.LiteLoader.plugins[pluginSlug]) {
-    sendMsg({
-      code: 400,
-      msg: 'No such plugin',
-    });
-    return ws.close();
-  }
-
-  ws.on('message', (_msg: string) => {
-    let msg = _msg;
-
-    try {
-      msg = JSON.parse(_msg);
-    } catch (e) {
-      msg = _msg;
-    }
-
-    console.log(msg);
-    sendMsg(msg);
-  });
-
-  ws.on('close', () => {
-    console.log('IPC WebSocket disconnected');
-  });
+export const IPCHandler = (msg: any, sendMsg: (data: any) => void) => {
+  console.log(msg);
+  sendMsg(msg);
 }
--- src/main/express/websocket.ts
+++ src/main/express/websocket.ts
@@ -2,5 +2,29 @@ import { Application } from 'express-ws';
 import { IPCHandler } from './controllers';
 
 export const useWebSocket = (app: Application) => {
-  app.ws('/ipc', IPCHandler);
+  app.ws('/ipc', (ws, req) => {
+    ws.on('message', async (msgRaw: string) => {
+      const sendMsg = (data: any) => ws.send(JSON.stringify(data));
+      let msg;
+
+      try {
+        msg = JSON.parse(msgRaw);
+        await IPCHandler(msg, sendMsg);
+      } catch (e) {
+        console.log('Error processing WebSocket message');
+        console.error(e);
+      }
+    });
+
+    ws.on('error', (e: any) => {
+      console.log('Error handling WebSocket connection');
+      console.error(e);
+    });
+
+    ws.on('close', () => {
+      console.log('Disconnected');
+    });
+
+    console.log('Connected');
+  });
 };
