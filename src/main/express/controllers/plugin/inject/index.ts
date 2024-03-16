738ce05d9036371a5a083bd3b4162ac2663b0bf5
Misa Liu <misaliu@misaliu.top>
2024-03-16 17:09:08 +0800

    feat: Add styles to iframe

--- src/main/express/controllers/plugin/inject/index.ts
+++ src/main/express/controllers/plugin/inject/index.ts
@@ -1,7 +1,19 @@
+import InjectStyleColorRaw from './color.css?raw';
+import InjectStyleRaw from './style.css?raw';
 import InjectIPCRaw from './ipc.js?raw';
 import InjectIframeRaw from './iframe.js?raw';
 import InjectLLAPIRaw from './llapi.js?raw';
 import { Request, Response } from 'express';
+
+export const GetStyleColor = (req: Request, res: Response) => {
+  res.set('Content-Type', 'text/css')
+    .send(InjectStyleColorRaw);
+}
+
+export const GetStyle = (req: Request, res: Response) => {
+  res.set('Content-Type', 'text/css')
+    .send(InjectStyleRaw);
+}
 
 export const GetIPC = (req: Request, res: Response) => {
   res.set('Content-Type', 'text/javascript')
--- src/main/express/routes/plugin/inject.ts
+++ src/main/express/routes/plugin/inject.ts
@@ -3,6 +3,8 @@ import { Plugin } from '@/main/express/controllers';
 
 const router = Router();
 
+router.get('/color.css', Plugin.Inject.GetStyleColor);
+router.get('/style.css', Plugin.Inject.GetStyle);
 router.get('/ipc.js', Plugin.Inject.GetIPC);
 router.get('/iframe.js', Plugin.Inject.GetJs);
 router.get('/llapi.js', Plugin.Inject.GetLLAPI);
