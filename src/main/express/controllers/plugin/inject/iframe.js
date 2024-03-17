534c5794f307994bb1203ed5013e24e9e61ea2ad
Misa Liu <misaliu@misaliu.top>
2024-03-17 17:09:34 +0800

    style: Moving code

--- src/main/express/controllers/plugin/inject/iframe.js
+++ src/main/express/controllers/plugin/inject/iframe.js
@@ -1,6 +1,40 @@
 ; 'use strict';
 
 (() => {
+  const ipcWs = new IPCWebSocket();
+
+  const FakeElectron = {
+    ipcRenderer: {
+      invoke: (channel, ...args) => {
+        console.log('[Electron][IPC][Invoke]', channel, args);
+        return ipcWs.invoke(channel, ...args);
+      },
+      send: (channel, ...args) => {
+        console.log('[Electron][IPC][Send]', channel, args);
+        return ipcWs.send(channel, ...args);
+      },
+      on: (channel, listener) => {
+        console.log('[Electron][IPC][On]', channel, listener);
+        return ipcWs.on(channel, listener);
+      },
+      off: (channel, listener) => {
+        console.log('[Electron][IPC][Off]', channel, listener);
+        return ipcWs.off(channel, listener);
+      }
+    },
+    contextBridge: {
+      exposeInMainWorld: (slug, obj) => {
+        window[slug] = { ...obj };
+      }
+    }
+  };
+
+  const FakeRequire = (moduleName) => {
+    if (moduleName === 'electron') return FakeElectron;
+    else return import(moduleName);
+  };
+  window.__FAKE_REQUIRE__ = FakeRequire;
+
   const parseLocalScheme = (url, pluginPath) => {
     if (url.indexOf('local:///') !== 0) return url;
     let result = url.replace('local:///', '');
@@ -8,68 +42,37 @@
     return result;
   };
 
+  // Hook fetch
+  const RealFetch = window.fetch;
+  const FakeFetch = (...args) => {
+    const [originUrl, originConfig = {}, ...originArgs] = args;
+
+    if (originUrl.indexOf('local:///') === 0) {
+      return RealFetch(parseLocalScheme(originUrl, originPath.plugin), originConfig, ...originArgs);
+    }
+
+    if (originUrl.indexOf(window.location.origin) !== 0) {
+      return RealFetch(
+        `${window.location.origin}/plugin/${pluginSlug}/proxy/`,
+        {
+          ...originConfig,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            url: originUrl,
+          }),
+        }
+      );
+    }
+
+    return RealFetch(originUrl, originConfig, ...originArgs);
+  }
+  window.fetch = FakeFetch;
+
   const loadPlugin = async (pluginSlug) => {
     const { manifest: pluginMeta, path: originPath } = LiteLoader.plugins[pluginSlug];
-    const ipcWs = new IPCWebSocket();
-    const FakeElectron = {
-      ipcRenderer: {
-        invoke: (channel, ...args) => {
-          console.log('[Electron][IPC][Invoke]', channel, args);
-          return ipcWs.invoke(channel, ...args);
-        },
-        send: (channel, ...args) => {
-          console.log('[Electron][IPC][Send]', channel, args);
-          return ipcWs.send(channel, ...args);
-        },
-        on: (channel, listener) => {
-          console.log('[Electron][IPC][On]', channel, listener);
-          return ipcWs.on(channel, listener);
-        },
-        off: (channel, listener) => {
-          console.log('[Electron][IPC][Off]', channel, listener);
-          return ipcWs.off(channel, listener);
-        }
-      },
-      contextBridge: {
-        exposeInMainWorld: (slug, obj) => {
-          window[slug] = { ...obj };
-        }
-      }
-    };
-    const FakeRequire = (moduleName) => {
-      if (moduleName === 'electron') return FakeElectron;
-      else return import(moduleName);
-    }
-    window.__FAKE_REQUIRE__ = FakeRequire;
-
-    // Hook fetch
-    const RealFetch = window.fetch;
-    const FakeFetch = (...args) => {
-      const [originUrl, originConfig = {}, ...originArgs] = args;
-
-      if (originUrl.indexOf('local:///') === 0) {
-        return RealFetch(parseLocalScheme(originUrl, originPath.plugin), originConfig, ...originArgs);
-      }
-
-      if (originUrl.indexOf(window.location.origin) !== 0) {
-        return RealFetch(
-          `${window.location.origin}/plugin/${pluginSlug}/proxy/`,
-          {
-            ...originConfig,
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              url: originUrl,
-            }),
-          }
-        );
-      }
-
-      return RealFetch(originUrl, originConfig, ...originArgs);
-    }
-    window.fetch = FakeFetch;
 
     // MutationObserver Html
     const observer = new MutationObserver((mutationsList) => {
