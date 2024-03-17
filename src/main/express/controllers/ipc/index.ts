a1fbf5de99917bc28a235fc6971dbef0913f3fad
Misa Liu <misaliu@misaliu.top>
2024-03-17 16:45:53 +0800

    refactor: IPC WebSocket router

--- src/main/express/controllers/ipc/index.ts
+++ src/main/express/controllers/ipc/index.ts
@@ -1,4 +1,5 @@
 import { IpcApiInvoke, IpcApiSend } from '@/main/helper/ipcHook';
+import { WebsocketRequestHandler } from 'express-ws';
 
 interface IWebSocketMessage {
   type: string,
@@ -9,28 +10,61 @@ interface IWebSocketMessage {
   echo?: any, // Optional
 }
 
-export const IPCHandler = ({ type, channel, params = [], echo }: IWebSocketMessage, sendMsg: (data: any) => void) => {
-  const result: IWebSocketMessage = { type, channel };
+const IPCEventListeners: Array<{
+  channel: string,
+  id: string,
+}> = [];
 
-  if (echo !== undefined || echo !== null) {
-    result.echo = echo;
-  }
+export const IPCWebSocketHandler: WebsocketRequestHandler = (ws, req) => {
+  const sendMsg = (data: any) => ws.send(JSON.stringify(data));
+  const IPCHandler = ({ type, channel, params = [], echo }: IWebSocketMessage) => {
+    const result: IWebSocketMessage = { type, channel };
+  
+    if (echo !== undefined || echo !== null) {
+      result.echo = echo;
+    }
+  
+    console.log({ type, channel, params, echo });
+    if (type === 'ping') {
+      result.type = 'pong';
+      sendMsg(result);
+    } else if (type === 'invoke') {
+      IpcApiInvoke(channel, params, (data) => {
+        result.data = data;
+        sendMsg(result);
+      }, (e) => {
+        result.error = e;
+        sendMsg(result);
+      });
+    } else if (type === 'send') {
+      IpcApiSend(channel, params);
+      sendMsg(result);
+    } else if (type === 'on') {
+  
+    }
+    // sendMsg(result);
+  };
 
-  console.log({ type, channel, params, echo });
-  if (type === 'ping') {
-    result.type = 'pong';
-    sendMsg(result);
-  } else if (type === 'invoke') {
-    IpcApiInvoke(channel, params, (data) => {
-      result.data = data;
-      sendMsg(result);
-    }, (e) => {
-      result.error = e;
-      sendMsg(result);
-    });
-  } else if (type === 'send') {
-    IpcApiSend(channel, params);
-    sendMsg(result);
-  }
-  // sendMsg(result);
-}
+  ws.on('message', async (msgRaw: string) => {
+    let msg;
+
+    try {
+      msg = JSON.parse(msgRaw);
+      await IPCHandler(msg);
+    } catch (e) {
+      console.log('Error processing WebSocket message');
+      console.error(e);
+    }
+  });
+
+  ws.on('error', (e: any) => {
+    console.log('Error handling WebSocket connection');
+    console.error(e);
+  });
+
+  ws.on('close', () => {
+    console.log('Disconnected');
+  });
+
+  console.log('Connected');
+};
