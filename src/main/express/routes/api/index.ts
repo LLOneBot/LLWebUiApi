b98d7e40d0420a342e3de9fb5cd4d1b396bc4b48
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-17 16:30:21 +0800

    feat:router panel

--- src/main/express/routes/api/index.ts
+++ src/main/express/routes/api/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { FileRouter } from './files';
+import { PanelRouter } from './panel';
 
 const router = Router();
 
 router.use('/files', FileRouter);
-
+router.use('/panel', PanelRouter);
 export { router as APIRouter };
