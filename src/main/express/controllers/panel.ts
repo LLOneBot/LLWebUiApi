ca46d0bde08c9ad55842c608631b30f8ebbd7829
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-17 16:35:29 +0800

    refactor:router

--- src/main/express/controllers/panel.ts
+++ src/main/express/controllers/panel.ts
@@ -3,3 +3,7 @@ import { Request, Response } from 'express';
 export function getWebState(_req: Request, res: Response) {
     res.json(DataClass.getInstance().get("WebUiApiState"));
 }
+export function getQQLoginQRcode(_req: Request, res: Response){
+    const base64Image = DataClass.getInstance().get("QRCODE_BASE64");
+    res.json( { base64Image: base64Image });
+}
--- src/main/express/routes/api/panel.ts
+++ src/main/express/routes/api/panel.ts
@@ -2,4 +2,5 @@ import { Router } from 'express';
 import { Panel } from '../../controllers';
 const router = Router();
 router.get("/getWebState", Panel.getWebState)
+router.get("/getQQLoginQRcode", Panel.getQQLoginQRcode)
 export { router as PanelRouter };
