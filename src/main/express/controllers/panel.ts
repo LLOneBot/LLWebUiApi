5f2ad44f8ffe7ecae07c4e43edf27d6b36ace754
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-17 16:58:08 +0800

    feat: check getQQLoginQRcode

--- src/main/express/controllers/panel.ts
+++ src/main/express/controllers/panel.ts
@@ -3,7 +3,14 @@ import { Request, Response } from 'express';
 export function getWebState(_req: Request, res: Response) {
     res.json(DataClass.getInstance().get("WebUiApiState"));
 }
-export function getQQLoginQRcode(_req: Request, res: Response){
+export function getQQLoginQRcode(_req: Request, res: Response) {
     const base64Image = DataClass.getInstance().get("QRCODE_BASE64");
-    res.json( { base64Image: base64Image });
+    if (base64Image === "") {
+        return res.status(400)
+            .json({
+                msg: 'Cant Get QRcode'
+            });
+    } else {
+        res.json({ base64Image: base64Image });
+    }
 }
