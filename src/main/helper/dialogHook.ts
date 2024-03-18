c3c7cd37d7168db01c87ef94c3ba06e1d6a5f00e
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-18 16:00:51 +0800

    feat:hookType

--- src/main/helper/dialogHook.ts
+++ src/main/helper/dialogHook.ts
@@ -1,9 +1,12 @@
 import { OpenDialogOptions, dialog } from "electron"
 
+
+type showOpenDialogCallBack = (options: OpenDialogOptions) => Promise<Electron.OpenDialogReturnValue> | Electron.OpenDialogReturnValue;
 export const initDialogHook = () => {
     const originshowOpenDialog = dialog.showOpenDialog;
     (dialog.showOpenDialog as any) = async function (options: OpenDialogOptions) {
         console.log("showOpenDialog", options);
+        // Hook 未实现仅实现监测
         return originshowOpenDialog(options);
     }
 }
