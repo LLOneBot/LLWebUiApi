293723bdd3adc2d2590a4ebb63878da4ec8098dc
Misa Liu <misaliu@misaliu.top>
2024-03-18 20:28:18 +0800

    feat: Add basic JWT auth middleware

--- src/main/express/middleware.ts
+++ src/main/express/middleware.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import { expressjwt, Request } from 'express-jwt';
 import { resolve } from 'path';
 import * as Utils from '@/main/helper/utils';
+import { Response, NextFunction } from 'express';
 import { Application } from 'express-ws';
 
 
@@ -9,4 +11,21 @@ export const useMiddleware = (app: Application) => {
   app.use(express.urlencoded({ extended: false }));
 
   app.use('/static', express.static(resolve(Utils.PLUGIN_DIR, './static')));
+
+  app.use(['/api', '/plugin', '/ipc'], expressjwt({
+    secret: '1145141919810', // XXX: Change it before release
+    algorithms: [ 'HS256' ],
+  }).unless({
+    path: [
+      '/api/auth/login',
+    ],
+  }), (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err.name === 'UnauthorizedError' || !req.auth || !req.auth.admin) {
+      return res.status(401)
+        .json({
+          msg: 'Not logged in or auth expired',
+        });
+    }
+    next();
+  });
 }
