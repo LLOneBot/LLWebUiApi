28764ceb15719e40b6a1834bbe93cdc182247f0c
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-18 20:28:37 +0800

    refactor:showDialogHook

--- src/main/helper/dialogHook.ts
+++ src/main/helper/dialogHook.ts
@@ -1,39 +1,32 @@
 import { OpenDialogOptions, dialog } from "electron"
 
 type showOpenDialogCallBack = (options: OpenDialogOptions) => Promise<Electron.OpenDialogReturnValue> | Electron.OpenDialogReturnValue;
-let showDialogList: showOpenDialogCallBack[] = [];
-export const addShowDialogHook = (callback: showOpenDialogCallBack) => {
-    for (let showDialogCallFunc of showDialogList) {
-        if (showDialogCallFunc === callback) {
-            return false;
-        }
+let showDialogCallBackFunc: showOpenDialogCallBack | undefined = undefined;
+export const hasShowDialogHook = () => {
+    if (showDialogCallBackFunc) {
+        return false;
+    } else {
+        return true;
     }
-    showDialogList.push(callback);
+}
+export const setShowDialogHook = (callback: showOpenDialogCallBack) => {
+    showDialogCallBackFunc = callback;
     return true;
 }
-export const removeShowDialogHook = (callback: showOpenDialogCallBack) => {
-    let newShowDialogList: showOpenDialogCallBack[] = [];
-    for (let showDialogCallFunc of showDialogList) {
-        if (showDialogCallFunc === callback) {
-            return false;
-        } else {
-            newShowDialogList.push(showDialogCallFunc);
-        }
-    }
-    newShowDialogList == showDialogList
+export const removeShowDialogHook = () => {
+    showDialogCallBackFunc = undefined;
     return true;
 }
 export const initDialogHook = () => {
     const originshowOpenDialog = dialog.showOpenDialog;
     (dialog.showOpenDialog as any) = async function (options: OpenDialogOptions) {
-        if (showDialogList.length > 0) {
-            let ret = showDialogList[showDialogList.length - 1](options);
+        if (showDialogCallBackFunc) {
+            let ret = showDialogCallBackFunc(options);
             console.log("showOpenDialogHook", ret);
             if (ret instanceof Promise) {
                 //showDialogList.pop();
                 return ret;
             }
-            showDialogList.pop();
             return new Promise<Electron.OpenDialogReturnValue>((resolve, _reject) => {
                 resolve(ret);
             })
