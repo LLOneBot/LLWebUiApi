831ced8655e41b896c6835dc0c74523e072992f2
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-18 15:50:46 +0800

    feat:hook log

--- src/main/helper/dialogHook.ts
+++ src/main/helper/dialogHook.ts
@@ -3,6 +3,7 @@ import { dialog } from "electron"
 export const initDialogHook = () => {
     const originshowOpenDialog = dialog.showOpenDialog;
     (dialog.showOpenDialog as any) = async function (browserWindow: Electron.BrowserWindow, options: Electron.OpenDialogOptions) {
+        console.log("showOpenDialog", browserWindow, options);
         return originshowOpenDialog(browserWindow, options);
     }
 }
