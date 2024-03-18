f310fd08abf6184ea3da52d30c82aae3bfc27d4a
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-18 16:09:08 +0800

    feat:dialog hook reg

--- src/main/helper/dialogHook.ts
+++ src/main/helper/dialogHook.ts
@@ -1,6 +1,6 @@
 import { OpenDialogOptions, dialog } from "electron"
 
-
+// Hook应该加入一个5min时间设置 及时清理
 type showOpenDialogCallBack = (options: OpenDialogOptions) => Promise<Electron.OpenDialogReturnValue> | Electron.OpenDialogReturnValue;
 let showDialogList: showOpenDialogCallBack[] = [];
 export const addShowDialogHook = (callback: showOpenDialogCallBack) => {
@@ -10,7 +10,11 @@ export const addShowDialogHook = (callback: showOpenDialogCallBack) => {
 export const initDialogHook = () => {
     const originshowOpenDialog = dialog.showOpenDialog;
     (dialog.showOpenDialog as any) = async function (options: OpenDialogOptions) {
-        console.log("showOpenDialog", options);
+        if (showDialogList.length > 0) {
+            let ret = showDialogList[showDialogList.length](options)
+            showDialogList.pop();
+            return ret;
+        }
         return originshowOpenDialog(options);
     }
 }
