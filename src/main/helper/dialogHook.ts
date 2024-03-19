7e065695b59e12fab03ab9d495e2e68dfba14e5d
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-19 12:40:09 +0800

    chore:remove log

--- src/main/helper/dialogHook.ts
+++ src/main/helper/dialogHook.ts
@@ -23,7 +23,7 @@ export const initDialogHook = () => {
     (dialog.showOpenDialog as any) = async function (options: OpenDialogOptions) {
         if (showDialogCallBackFunc) {
             let ret = showDialogCallBackFunc(options);
-            CoreLog.getInstance().pushLog(LogLevel.Debug, "showOpenDialogHook", ret);
+            //CoreLog.getInstance().pushLog(LogLevel.Debug, "showOpenDialogHook", ret);
             if (ret instanceof Promise) {
                 return ret;
             }
