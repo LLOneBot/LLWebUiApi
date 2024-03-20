5e8eb06e474ace79bc77680e6e33b5c0ed1bf865
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-20 21:39:36 +0800

    style:refactor

---
+++ src/main/helper/update.ts
@@ -0,0 +1,6 @@
+import { version } from "@/common/version";
+import * as path from "node:path";
+import * as fs from "node:fs";
+
+const downloadMirrorHosts = ["https://mirror.ghproxy.com/"];
+const checkVersionMirrorHosts = ["https://521github.com"];
--- src/main/plugin/plugin.ts
+++ src/main/plugin/plugin.ts
@@ -18,31 +18,31 @@ export class WebPlugin {
         return WebPlugin.CurrentInstance;
     }
     private listFile(filePath: string) {
-            let data = new Array<FileStateApi>();
-            // 获取文件列表
-            const files = fs.readdirSync(filePath);
-            // 遍历文件列表
-            files.forEach((filename) => {
-                const filedir = path.resolve(filePath, filename);
-                const stats = fs.statSync(filedir);
-                const isFile = stats.isFile();
-                const isDir = stats.isDirectory(); // 是文件夹
-                let filetype: FileType = FileType.ERROR;
-                if (isFile) {
-                    filetype = FileType.FILE;
-                }
-                else if (isDir) {
-                    filetype = FileType.PATH;
-                }
-                let pushdata: FileStateApi = stats;
-                pushdata.filename = filename;
-                pushdata.filetype = filetype;
-                data.push(pushdata);
-            });
-            return data;
-        }
+        let data = new Array<FileStateApi>();
+        // 获取文件列表
+        const files = fs.readdirSync(filePath);
+        // 遍历文件列表
+        files.forEach((filename) => {
+            const filedir = path.resolve(filePath, filename);
+            const stats = fs.statSync(filedir);
+            const isFile = stats.isFile();
+            const isDir = stats.isDirectory(); // 是文件夹
+            let filetype: FileType = FileType.ERROR;
+            if (isFile) {
+                filetype = FileType.FILE;
+            }
+            else if (isDir) {
+                filetype = FileType.PATH;
+            }
+            let pushdata: FileStateApi = stats;
+            pushdata.filename = filename;
+            pushdata.filetype = filetype;
+            data.push(pushdata);
+        });
+        return data;
+    }
     public setWhiteList(data: string[]) {
-        this.whiteList = ["WebUiApi", ...data];
+        this.whiteList = [...data];
     }
     public loadPluginInfo(path: string) {
         let fileList: FileStateApi[] = this.listFile(path);
